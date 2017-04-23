f15ad1db9dc2172110eadcb9a0916b82bd9d498a
Richie Casto <Richie Casto>
2017-04-23 10:45:27 -0700

    bind this, via arrow function

--- activityMonitor.js
+++ activityMonitor.js
@@ -10,8 +10,8 @@ class ActivityMonitor extends events.EventEmitter {
         this.config.inactivityTimeoutInMs = 
             this.config.inactivityTimeoutInMs || 1800000; // defaults to 30 minutes
 
-        process.on('exit', this.clearActivity);
-        process.on('SIGINT', this.clearActivity);
+        process.on('exit', () => this.clearActivity());
+        process.on('SIGINT', () => this.clearActivity());
     }
     /*
         activity is of the following form:
